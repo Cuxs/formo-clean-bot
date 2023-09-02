10c2e3d52e04b41836a3ed85cb00c4496705d9d2
Mariano Fliri <riano.qx@gmail.com>
2023-09-02 16:46:09 -0300

    update pg connection string

--- knexfile.js
+++ knexfile.js
@@ -39,18 +39,6 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection:process.env.PG_CONNECTION_STRING,
   }
-
 };
